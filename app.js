45c134e1a6b26b55f2518ef079d288986aa728c3
CarlaCarrillo <ccarrillo1616@gmail.com>
2019-05-16 16:13:08 -0500

    ejercicio cookies

--- app.js
+++ app.js
@@ -1,49 +1,35 @@
 var express = require('express');
 var app = express();
-var path = require ('path')
+var path = require ('path');
+var bodyParser = require ('body-parser');
+var cookieParser = require ('cookie-parser');
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/hellow', function (req, res) {
-  res.send('Hello World!');
+let books = [];
+
+
+
+//ejercicio 4
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.get("/books", (req, res) => {
+  res.status(200).send({data: books});
+  console.log(req.cookies)
 });
 
-// respond with "bye bye" when a GET request is made to the homepage
-app.get('/bye', function (req, res) {
-    res.send('bye bye');
-  });
+app.post('/books',function (req, res) {
+ console.log(req.body)
+ console.log(req.cookies)
+ 
+  if (req.body.nombrebook && req.body.autorbook) {
+    books.push(req.body);
+    res.status(201).send("sí quedó");
+  }else {
+    res.status(400).send({error:"necesitar agregar el nombre y el autor del libro"})
+  }
+  })
 
-//app.use(express.static('assets')) y se abre en navegador con http://localhost:3000/assets/nombre de imagen.jpeg
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-
-
-//ejercicio 2
-app.get('/p1', function (req,res) {
-  res.send("estas en p1")
-});
-
-app.post('/p2', function (req,res) {
-  res.send("Sorry, we cannot find that!")
-});
-
-app.path('/p3', function (req,res)  {
-  res.status(p3).sendFile('/assets/puppy-1.png')
-});
-
-
-//ejercicio 3 middleware
-app.all('/', function (req, res, next) {
-  console.log('estamos haciendo petición')
-  next()
-});
-
-app.get('/hi', function (req, res) {
-  res.send('holi');
-});
-
-app.get('/adiosin', function (req, res) {
-  res.send('lo lograste');
-});
 
 
 
