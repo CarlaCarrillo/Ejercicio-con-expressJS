89fcebf994cf82b0c82b43f689d4512b1c9e5524
CarlaCarrillo <ccarrillo1616@gmail.com>
2019-05-16 12:27:54 -0500

    ejercicio3

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var path = require ('path')
 
-
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/hellow', function (req, res) {
   res.send('Hello World!');
@@ -12,6 +11,10 @@ app.get('/hellow', function (req, res) {
 app.get('/bye', function (req, res) {
     res.send('bye bye');
   });
+
+//app.use(express.static('assets')) y se abre en navegador con http://localhost:3000/assets/nombre de imagen.jpeg
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
 
 
 //ejercicio 2
@@ -23,13 +26,27 @@ app.post('/p2', function (req,res) {
   res.send("Sorry, we cannot find that!")
 });
 
-app.path('/p3', (req,res)  {
+app.path('/p3', function (req,res)  {
   res.status(p3).sendFile('/assets/puppy-1.png')
 });
 
 
-//app.use(express.static('assets')) y se abre en navegador con http://localhost:3000/assets/nombre de imagen.jpeg
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+//ejercicio 3 middleware
+app.all('/', function (req, res, next) {
+  console.log('estamos haciendo petición')
+  next()
+});
+
+app.get('/hi', function (req, res) {
+  res.send('holi');
+});
+
+app.get('/adiosin', function (req, res) {
+  res.send('lo lograste');
+});
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
